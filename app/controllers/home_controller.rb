d313325209cf3248511169f801275bd1a0638d12
Daniel Jaouen <dcj24@cornell.edu>
2014-06-24 22:29:38 -0400

    Move user logic into User model.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,5 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user = current_user
-    @friendships = @user.friendships.where(pending: false)
-    @pending_friendships = @user.friendships.where(pending: true)
-    @ratings = Rating.where(rater: @user)
-    @recommendations = Recommendation.where(recommendee: @user)
-    @recommended = Recommendation.where(recommender: @user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,24 @@ class User < ActiveRecord::Base
 
   has_many :recommendations, class_name: 'Recommendation', foreign_key: 'recommender_id'
   has_many :recommended, class_name: 'Recommendation', foreign_key: 'recommendee_id'
+
+  def current_friendships
+    friendships.where(pending: false)
+  end
+
+  def pending_friendships
+    friendships.where(pending: true)
+  end
+
+  def ratings
+    Rating.where(rater: self)
+  end
+
+  def recommendations
+    Recommendation.where(recommendee: self)
+  end
+
+  def recommended
+    Recommendation.where(recommender: self)
+  end
 end

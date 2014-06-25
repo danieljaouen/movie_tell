71a12cf3a33a8cda987618d6f52835497b5a969f
Daniel Jaouen <dcj24@cornell.edu>
2014-06-24 22:33:31 -0400

    Move user logic into User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ActiveRecord::Base
   has_many :friendships
   has_many :friends, through: :friendships
 
-  has_many :ratings
+  has_many :ratings, foreign_key: :rater_id
 
-  has_many :recommendations, class_name: 'Recommendation', foreign_key: 'recommender_id'
-  has_many :recommended, class_name: 'Recommendation', foreign_key: 'recommendee_id'
+  has_many :recommendations, class_name: 'Recommendation', foreign_key: 'recommendee_id'
+  has_many :recommended, class_name: 'Recommendation', foreign_key: 'recommender_id'
 
   def current_friendships
     friendships.where(pending: false)
@@ -16,17 +16,5 @@ class User < ActiveRecord::Base
 
   def pending_friendships
     friendships.where(pending: true)
-  end
-
-  def ratings
-    Rating.where(rater: self)
-  end
-
-  def recommendations
-    Recommendation.where(recommendee: self)
-  end
-
-  def recommended
-    Recommendation.where(recommender: self)
   end
 end

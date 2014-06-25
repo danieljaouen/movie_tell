3bdf3d122c6bddaa15c32b131af098e00b54c8de
Daniel Jaouen <dcj24@cornell.edu>
2014-06-24 23:39:05 -0400

    Refactor Recommendation controller.

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -4,14 +4,8 @@ class RecommendationsController < ApplicationController
 
   def create
     rottentomatoes_id = params[:rottentomatoes_id]
-    friends = []
-    params.each do |key, val|
-      if key.to_s.start_with? 'friend-'
-        friend = User.find_by(id: val.to_i)
-        friends << friend
-      end
-    end
-    friends = friends.reject(&:nil?)
+
+    friends = extract_friends
     friends.each do |friend|
       recommendation = Recommendation.new(rottentomatoes_id: rottentomatoes_id,
                                           recommender: current_user,
@@ -19,12 +13,8 @@ class RecommendationsController < ApplicationController
       recommendation.save
     end
 
-    respond_to do |format|
-      format.html do
-        redirect_to movie_path(id: rottentomatoes_id),
-          notice: 'Recommendation was successfully created.'
-      end
-    end
+    redirect_to movie_path(id: rottentomatoes_id),
+      notice: 'Recommendation was successfully created.'
   end
 
   def destroy
@@ -41,5 +31,17 @@ class RecommendationsController < ApplicationController
 
   def recommendation_params
     params.permit(:rottentomatoes_id, :recommendee_id)
+  end
+
+  def extract_friends
+    friends = []
+    params.each do |key, val|
+      next if !key.to_s.start_with? 'friend-'
+
+      friend = User.find_by(id: val.to_i)
+      friends << friend if current_user.friends.include?(friend)
+    end
+
+    friends
   end
 end

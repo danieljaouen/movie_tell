13f9caa03fc674716d4a6d1ebcb307a15ec18bc6
Daniel Jaouen <dcj24@cornell.edu>
2014-06-30 20:44:25 -0400

    Add unconfirmed_email to users.

---
+++ db/migrate/20140701004215_add_unconfirmed_email_to_devise.rb
@@ -0,0 +1,9 @@
+class AddUnconfirmedEmailToDevise < ActiveRecord::Migration
+  def self.up
+    add_column :users, :unconfirmed_email, :string
+  end
+
+  def self.down
+    remove_column :users, :unconfirmed_email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630224245) do
+ActiveRecord::Schema.define(version: 20140701004215) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20140630224245) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
